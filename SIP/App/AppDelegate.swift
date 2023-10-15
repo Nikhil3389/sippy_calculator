87ed04742b326a44720e98d43e099744aed3d341
Nikhil3389 <71307854+Nikhil3389@users.noreply.github.com>
2023-10-16 01:30:14 +0530

    Update AppDelegate.swift

--- SIP/App/AppDelegate.swift
+++ SIP/App/AppDelegate.swift
@@ -7,13 +7,11 @@
 
 import Foundation
 import SwiftUI
-import FirebaseCore
 import SwiftRater
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
         SwiftRater.daysUntilPrompt = 3
         SwiftRater.usesUntilPrompt = 10
         SwiftRater.significantUsesUntilPrompt = 3
